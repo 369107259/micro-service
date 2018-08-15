28e9ff933aed2a3dd5c2e20843683bedcfcf34cf
huangyong <369107259@qq.com>
2018-08-15 18:30:26 +0800

    commit

--- lie-server-consumer/src/main/java/com/cn/controller/HelloController.java
+++ lie-server-consumer/src/main/java/com/cn/controller/HelloController.java
@@ -1,8 +1,6 @@
 package com.cn.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -14,16 +12,10 @@ import org.springframework.web.client.RestTemplate;
 public class HelloController {
 
     @Autowired
-    private LoadBalancerClient loadBalancerClient;
-
-    @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/consumer")
     public String hello(){
-        ServiceInstance serviceInstance = loadBalancerClient.choose("lie-server-client");
-        String url = "http://localhost:" + serviceInstance.getPort() + "/hello";
-        System.out.println(url);
-        return  restTemplate.getForObject(url,String.class);
+        return  restTemplate.getForObject("http://lie-server-client/hello",String.class);
     }
 }
