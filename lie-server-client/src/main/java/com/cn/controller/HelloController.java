8ff55ebd68c844e823d2646d89958aff2b419c64
huangyong <369107259@qq.com>
2018-08-17 18:08:15 +0800

    commit

--- lie-server-client/src/main/java/com/cn/controller/HelloController.java
+++ lie-server-client/src/main/java/com/cn/controller/HelloController.java
@@ -3,13 +3,16 @@ package com.cn.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /***
  * @author huangyong
  */
+@RefreshScope
 @RestController
 public class HelloController {
     private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
@@ -19,6 +22,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
+
         String services = "services:" + discoveryClient.getServices();
         logger.info(services);
         return services;
