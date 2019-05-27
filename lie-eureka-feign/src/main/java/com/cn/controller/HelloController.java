93e020ea851734b2645e507953d046b3878c8fa0
huangyong <369107259@qq.com>
2019-05-27 17:49:42 +0800

    #commit

--- lie-eureka-feign/src/main/java/com/cn/controller/HelloController.java
+++ lie-eureka-feign/src/main/java/com/cn/controller/HelloController.java
@@ -5,13 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 /***
  * @author
  */
 @RestController
 public class HelloController {
 
-    @Autowired
+    @Resource
     private HelloService helloService;
 
     @GetMapping("/hello")
